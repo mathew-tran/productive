16994626448e537b82ad6d68a9730f6813d0d135
Mathew Tran <mathew.tran.dev@gmail.com>
2024-10-14 14:33:47 -0700

    lerp color for progress

--- Scripts/Goal.gd
+++ Scripts/Goal.gd
@@ -4,24 +4,55 @@ var GoalName = "GoalName"
 
 @onready var GoalTitle = $HBoxContainer/VBoxContainer/GoalTitle
 @onready var HoursWorked = $HBoxContainer/VBoxContainer/HoursWorked
+@onready var CompletionProgress = $HBoxContainer/VBoxContainer/HBoxContainer/ProgressBar
 
-var Seconds = 0
-var GoalInHours = 0
+var Seconds = 0.0
+var GoalInHours = 1
 var GoalInMinutes = 0
+
+var bHasBeenCompleted = false
 
 func _ready():
 	Update()
+	Engine.time_scale = 60
+	Initialize()
+	
+func Initialize():
+	CompletionProgress.min_value = 0
+	
+	var seconds = 0
+	seconds += GoalInMinutes * 60
+	seconds += GoalInHours * 60 * 60
+	CompletionProgress.max_value = seconds
 	
 func Update():
 	GoalTitle.text = GoalName
 	HoursWorked.text = ConvertSecondsIntoText(Seconds) + "/" + GetGoalText()
+	CompletionProgress.value = Seconds	
+	
+	if Seconds >= CompletionProgress.max_value:
+		HoursWorked.modulate = Color.GREEN
+	elif Seconds < CompletionProgress.max_value:
+		HoursWorked.modulate = Color.WHITE
+		
+	var weight = CompletionProgress.value / CompletionProgress.max_value
+	CompletionProgress.modulate = lerp(Color.YELLOW, Color.GREEN, weight)
 	
 func GetGoalText():
-	return str(GoalInHours) + "h " + str(GoalInMinutes) + "m"
+	return GetTimeText(GoalInHours, GoalInMinutes)
+	
+	
+func GetTimeText(hours, minutes):
+	return str(round(hours)) + "h " + str(round(minutes)) + "m"
 	
 func ConvertSecondsIntoText(seconds):
 	
-	var result = str(seconds).format("%0.3f")
+	if seconds == 0:
+		return GetTimeText(0, 0)
+	var hours = int(seconds / 3600)
+	var minutes = int((int(seconds) % 3600) / 60)
+	
+	var result = GetTimeText(hours, minutes)
 
 	return result
 	
@@ -37,5 +68,8 @@ func _on_button_custom_press(bIsPlaying):
 
 
 func _on_timer_timeout():
-	Seconds += $Timer.wait_time
+	Seconds += $Timer.wait_time * 100
+	if bHasBeenCompleted == false and Seconds >= CompletionProgress.max_value:
+		bHasBeenCompleted = true
+		
 	Update()
